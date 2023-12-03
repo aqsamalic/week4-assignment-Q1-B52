2bb25855c9f02cedc52ababbd107c309d26a6ef8
aqsamalic <150602787+aqsamalic@users.noreply.github.com>
2023-12-03 11:10:19 +0500

    week4 assignment  Q1 B52

---
+++ assingment no 1/index.js
@@ -0,0 +1,45 @@
+"use strict";
+//exercise no :1
+let x = 5;
+if (x % 2 == 0) {
+    console.log("The number is Even");
+}
+else {
+    console.log("The number is Odd");
+}
+//Eercise no :2
+let age = 20;
+if (age >= 18) {
+    console.log("Person is eligible to vote");
+}
+else {
+    console.log("Person is not eligible to vote");
+}
+// Exercise no:3
+let n = -4;
+if (n < 0) {
+    console.log("The number is negative");
+}
+else if (n > 0) {
+    console.log("The number is positive");
+}
+else {
+    console.log("The number is Zero");
+}
+//Exercise no:4
+//let lenght:number=5
+//let width:number=7
+//function area( lenght:number,width:number){
+//  return lenght*width
+//console.log("Area of rectangle="+area())
+//Exercise:5
+let result = ((10 + 5) * 3 - 2) / (4 % 3) - 7;
+console.log("Result=", result);
+let lenght = 6;
+let width = 5;
+let area = lenght * width;
+console.log("area=", area);
+//Exercise no :6
+let a = 6;
+const volume = a * a * a;
+console.log("Volume of a cube=", volume);
---
+++ assingment no 1/index.ts
@@ -0,0 +1,45 @@
+//exercise no :1
+
+let x:number=5;
+if(x%2==0){
+    console.log("The number is Even");
+}
+else{
+    console.log("The number is Odd");
+}
+//Eercise no :2
+let age:number=20
+if( age >=18){
+console.log("Person is eligible to vote");
+}
+else{
+    console.log("Person is not eligible to vote");
+}
+// Exercise no:3
+
+let n:number=-4
+if(n<0){
+    console.log("The number is negative");
+}
+else if(n>0){
+    console.log("The number is positive");
+}
+else{
+    console.log("The number is Zero");
+}
+
+
+//Exercise:5
+let result=((10+5)*3-2)/(4%3)-7;
+console.log("Result=",result);
+
+let lenght:number =6
+let width:number=5
+let area=lenght*width;
+console.log("area=",area);
+
+//Exercise no :6
+
+let a:number=6
+const volume=a*a*a
+console.log("Volume of a cube=",volume);
